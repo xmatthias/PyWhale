df80271d656731fdecc5b80b4515b8cc8f8d8108
Matthias Voppichler <matthias.voppichler@gmail.com>
2017-12-09 16:52:48 +0100

    improve key loading

--- pywhale/trade/Api.py
+++ pywhale/trade/Api.py
@@ -14,14 +14,20 @@
 # General Public LICENSE along with krakenex. If not, see
 # <http://www.gnu.org/licenses/gpl-3.0.txt>.
 
-import sys
 
-
-class Api (object):
+class Api(object):
     """	Whaleclub.co cryptocurrency Exchange API Pyhon Client
         Connection Handler methods:"""
 
     verbose = False  # set to True if you get output twice
+
+    keydict = {
+        "BTC_demo_key": "",
+        "BTC_real_key": "",
+        "DASH_demo_key": "",
+        "DASH_real_key": "",
+    }
+    default_key = "BTC_demo_key"
 
     def __init__(self, start_url='https://api.whaleclub.co/v1/',
                  BTC_real_key='', BTC_demo_key='',
@@ -39,10 +45,10 @@ class Api (object):
 
         """
         self.start_url = start_url
-        self.BTC_demo_key = BTC_demo_key
-        self.BTC_real_key = BTC_real_key
-        self.DASH_demo_key = DASH_demo_key
-        self.DASH_real_key = DASH_real_key
+        self.keydict["BTC_demo_key"] = BTC_demo_key
+        self.keydict["BTC_real_key"] = BTC_real_key
+        self.keydict["DASH_demo_key"] = DASH_demo_key
+        self.keydict["DASH_real_key"] = DASH_real_key
         self.load_tokens()
 
     def load_tokens(self):
@@ -54,25 +60,27 @@ class Api (object):
             "BTC_real_key.txt",
             "DASH_demo_key.txt",
             "DASH_real_key.txt"]
-        keys = []
 
         # open files and save token
         for file in files:
-            f = open(file, 'r')
-            key = f.readline().rstrip('\n')
-            keys.append(key)
+            fil = open(file, 'r')
+            keyname = file[:-4]
+            if keyname in self.keydict:
+                self.keydict[keyname] = fil.readline().rstrip('\n')
 
-        # check if token exist
-        for key in keys:
-            if len(key) == 0:
-                error_message = '''Error, at least one API token is missing.
-Check that the API tokens in following files are correct and try again:
-BTC_demo_key.txt,BTC_real_key.txt,DASH_demo_key.txt,DASH_real_key.txt'''
-                print(error_message)
-                sys.exit(1)
+    def _updateKey(self, key):
+        if key is None:
+            key = self.default_key
 
-        # update token with token loaded
-        self.BTC_demo_key, self.BTC_real_key, self.DASH_demo_key, self.DASH_real_key = keys
+        # Return if key exists
+        if key in self.keydict:
+            return(True, self.keydict[key])
+
+        print("\nError, undefined key parameter ", key, ".")
+        print("enter an acctepted value for key parameter, "
+              "could either be 'BTC_real_key', 'BTC_demo_key', "
+              "'DASH_real_key' or 'DASH_demo_key' \n")
+        return(False, None)
 
     @staticmethod
     def _testsymbols(symb):
@@ -81,25 +89,3 @@ BTC_demo_key.txt,BTC_real_key.txt,DASH_demo_key.txt,DASH_real_key.txt'''
                   '5 elements at once. Lower your input number and retry\n')
             return False
         return True
-
-    def _updateKey(self, key):
-        if key is None:
-            key = self.default_key
-
-        # test if key parameter value is an accepted input
-        l = ['BTC_real_key', 'BTC_demo_key', 'DASH_real_key', 'DASH_demo_key']
-        if key in l:
-            i = l.index(key)
-            k = [
-                self.BTC_real_key,
-                self.BTC_demo_key,
-                self.DASH_real_key,
-                self.DASH_demo_key]
-            key = k[i]
-        else:
-            print("\nError, enter an acctepted value for key parameter, "
-                  "could either be 'BTC_real_key', 'BTC_demo_key', "
-                  "'DASH_real_key' or 'DASH_demo_key' \n")
-            return (False, key)
-
-        return (True, key)
